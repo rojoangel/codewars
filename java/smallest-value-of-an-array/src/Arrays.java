cc41e7c9f8cb72fe0fd8d340e564b9558949bccf
rojoangel <rojo.angel@gmail.com>
2015-08-29 22:44:20 +0200

    java - smallest-value-of-an-array - it. 2 - refactored by removing the smallestValue var

--- java/smallest-value-of-an-array/src/Arrays.java
+++ java/smallest-value-of-an-array/src/Arrays.java
@@ -1,5 +1,33 @@
-/**
- * Created by rojoa on 29/08/15.
- */
 public class Arrays {
+
+    public static final String INDEX = "index";
+    public static final String VALUE = "value";
+
+    public static int findSmallest(int[] ints, String index) {
+
+        validateParams(ints, index);
+
+        int smallestIndex = 0;
+        for (int i = 0; i < ints.length; i++) {
+            if (ints[i] < ints[smallestIndex]) {
+                smallestIndex = i;
+            }
+        }
+
+        return INDEX.equals(index) ? smallestIndex : ints[smallestIndex];
+    }
+
+    private static void validateParams(int[] ints, String index) {
+        if (ints == null) {
+            throw new IllegalArgumentException("ints argument cannot be null.");
+        }
+
+        if (ints.length == 0) {
+            throw new IllegalArgumentException("ints argument cannot be empty");
+        }
+
+        if (!(INDEX.equals(index) || VALUE.equals(index))) {
+            throw new IllegalArgumentException("index argument has to be 'index' or 'value'");
+        }
+    }
 }
