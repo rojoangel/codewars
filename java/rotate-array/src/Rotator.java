e89d0d85829befbcd8fcc8b01cc8059fc4e7c772
rojoangel <rojo.angel@gmail.com>
2015-09-01 22:49:28 +0200

    java - rotate-array - it. 2 - inline

--- java/rotate-array/src/Rotator.java
+++ java/rotate-array/src/Rotator.java
@@ -2,9 +2,8 @@ public class Rotator {
 
     public Object[] rotate(Object[] data, int n) {
         Object[] rotatedData = new Object[data.length];
-        int m = n % data.length;
         for (int i = 0; i < data.length; i++) {
-            rotatedData[(i + m)  % data.length < 0 ? ((i + m) % data.length) + data.length : (i + m)  % data.length] = data[i];
+            rotatedData[(i + n % data.length)  % data.length < 0 ? ((i + n % data.length) % data.length) + data.length : (i + n % data.length)  % data.length] = data[i];
         }
         return rotatedData;
     }
