4edf6e493aa44e7a5b06b0caf503de4e2ac3a38f
rojoangel <rojo.angel@gmail.com>
2015-09-01 23:33:18 +0200

    java - rotate-array - it. 3 - simplify

--- java/rotate-array/src/Rotator.java
+++ java/rotate-array/src/Rotator.java
@@ -3,7 +3,7 @@ public class Rotator {
     public Object[] rotate(Object[] data, int n) {
         Object[] rotatedData = new Object[data.length];
         for (int i = 0; i < data.length; i++) {
-            rotatedData[(i + n % data.length)  % data.length < 0 ? ((i + n % data.length) % data.length) + data.length : (i + n % data.length)  % data.length] = data[i];
+            rotatedData[(i + n % data.length + data.length)  % data.length] = data[i];
         }
         return rotatedData;
     }
