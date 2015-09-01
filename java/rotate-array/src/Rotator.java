fd7ded91725403a374c302f72e573e93ba18b6c0
rojoangel <rojo.angel@gmail.com>
2015-09-02 00:10:52 +0200

    java- rotate-arrey - it. 4 - beautify: remove extra lines and white spaces

--- java/rotate-array/src/Rotator.java
+++ java/rotate-array/src/Rotator.java
@@ -3,9 +3,8 @@ public class Rotator {
     public Object[] rotate(Object[] data, int n) {
         Object[] rotatedData = new Object[data.length];
         for (int i = 0; i < data.length; i++) {
-            rotatedData[(i + n % data.length + data.length)  % data.length] = data[i];
+            rotatedData[(i + n % data.length + data.length) % data.length] = data[i];
         }
         return rotatedData;
     }
-
 }
