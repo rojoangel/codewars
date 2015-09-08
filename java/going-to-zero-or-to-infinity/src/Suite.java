b653071f001612eb200887d7e1bf80ceaa440bc2
rojoangel <rojo.angel@gmail.com>
2015-09-08 21:29:01 +0200

    java - going-to-zero-or-infinity - it. 3 - optimized for performance: reduced number of loops

--- java/going-to-zero-or-to-infinity/src/Suite.java
+++ java/going-to-zero-or-to-infinity/src/Suite.java
@@ -1,27 +1,16 @@
 import java.math.BigDecimal;
-import java.math.BigInteger;
 
 public class Suite {
 
     public static double going(int n) {
-//         return factorialSum(n) / factorial(n);
-        BigDecimal bd = factorialSum(n).divide(factorial(n), BigDecimal.ROUND_FLOOR);
-        return bd.doubleValue();
-    }
-
-    public static BigDecimal factorial(int n) {
-        BigDecimal fact = BigDecimal.ONE;
+        BigDecimal fact = BigDecimal.ONE.setScale(6, BigDecimal.ROUND_UNNECESSARY);
+        BigDecimal sum = BigDecimal.ZERO.setScale(6, BigDecimal.ROUND_UNNECESSARY);
         for (int i = 1; i <= n; i++) {
             fact = fact.multiply(BigDecimal.valueOf(i));
+            sum = sum.add(fact);
         }
-        return fact.setScale(6, BigDecimal.ROUND_UNNECESSARY);
-    }
 
-    public static BigDecimal factorialSum(int n) {
-        BigDecimal sum = BigDecimal.ZERO;
-        for (int i = 1; i <= n; i++) {
-            sum = sum.add(factorial(i));
-        }
-        return sum.setScale(6, BigDecimal.ROUND_UNNECESSARY);
+        BigDecimal bd = sum.divide(fact, BigDecimal.ROUND_FLOOR);
+        return bd.doubleValue();
     }
 }
