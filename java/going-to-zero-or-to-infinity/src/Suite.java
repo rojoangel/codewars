e4bc3865f102df4b3029bc4396d97ec516d465c9
rojoangel <rojo.angel@gmail.com>
2015-09-08 20:47:11 +0200

    java - going-to-zero-or-to-infinity - it. 2 - passes local tests, but fails submission tests - need to handle big nums

--- java/going-to-zero-or-to-infinity/src/Suite.java
+++ java/going-to-zero-or-to-infinity/src/Suite.java
@@ -1,6 +1,27 @@
+import java.math.BigDecimal;
+
 public class Suite {
 
     public static double going(int n) {
-        // your code
+//         return factorialSum(n) / factorial(n);
+        BigDecimal bd = new BigDecimal(factorialSum(n) / factorial(n));
+        bd = bd.setScale(6, BigDecimal.ROUND_HALF_UP);
+        return bd.doubleValue();
+    }
+
+    public static double factorial(int n) {
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
+
+    public static double factorialSum(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += factorial(i);
+        }
+        return sum;
     }
 }
