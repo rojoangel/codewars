463e4fca917d550e8db21d855dc5bc801e8d6e5d
rojoangel <rojo.angel@gmail.com>
2015-09-08 21:18:49 +0200

    java - going-to-zero-or-to-infinity - it. 2 - passes local tests, but gives timeout when submitted

--- java/going-to-zero-or-to-infinity/src/Suite.java
+++ java/going-to-zero-or-to-infinity/src/Suite.java
@@ -1,27 +1,27 @@
 import java.math.BigDecimal;
+import java.math.BigInteger;
 
 public class Suite {
 
     public static double going(int n) {
 //         return factorialSum(n) / factorial(n);
-        BigDecimal bd = new BigDecimal(factorialSum(n) / factorial(n));
-        bd = bd.setScale(6, BigDecimal.ROUND_HALF_UP);
+        BigDecimal bd = factorialSum(n).divide(factorial(n), BigDecimal.ROUND_FLOOR);
         return bd.doubleValue();
     }
 
-    public static double factorial(int n) {
-        int fact = 1;
+    public static BigDecimal factorial(int n) {
+        BigDecimal fact = BigDecimal.ONE;
         for (int i = 1; i <= n; i++) {
-            fact *= i;
+            fact = fact.multiply(BigDecimal.valueOf(i));
         }
-        return fact;
+        return fact.setScale(6, BigDecimal.ROUND_UNNECESSARY);
     }
 
-    public static double factorialSum(int n) {
-        int sum = 0;
+    public static BigDecimal factorialSum(int n) {
+        BigDecimal sum = BigDecimal.ZERO;
         for (int i = 1; i <= n; i++) {
-            sum += factorial(i);
+            sum = sum.add(factorial(i));
         }
-        return sum;
+        return sum.setScale(6, BigDecimal.ROUND_UNNECESSARY);
     }
 }
