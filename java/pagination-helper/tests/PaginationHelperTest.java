1aff00cd6a85b296e5f23952decac84ff193ac65
rojoangel <rojo.angel@gmail.com>
2015-09-09 20:24:31 +0200

    java - pagination-helper - it. 7 - test for page count for empty collection

--- java/pagination-helper/tests/PaginationHelperTest.java
+++ java/pagination-helper/tests/PaginationHelperTest.java
@@ -10,18 +10,25 @@ public class PaginationHelperTest {
     @Test
     public void testItemCountNullCollection() throws Exception {
         PaginationHelper helper = new PaginationHelper(null, 4);
-        assertEquals("items count", 0, helper.itemCount());
+        assertEquals("items count for null collection", 0, helper.itemCount());
     }
 
     @Test
     public void testItemCountEmptyCollection() throws Exception {
         PaginationHelper helper = new PaginationHelper(Collections.emptyList(), 4);
-        assertEquals("items count", 0, helper.itemCount());
+        assertEquals("items count for empty collection", 0, helper.itemCount());
     }
 
     @Test
     public void testItemCount() throws Exception {
         PaginationHelper helper = new PaginationHelper(Arrays.asList('a', 'b', 'c', 'd', 'e', 'f'), 4);
-        assertEquals("items count", 6, helper.itemCount());
+        assertEquals("items count for non empty collection", 6, helper.itemCount());
     }
+
+    @Test
+    public void testPageCountEmptyCollection() throws Exception {
+        PaginationHelper helper = new PaginationHelper(Collections.emptyList(), 4);
+        assertEquals("page count for empty collection", 0, helper.pageCount());
+    }
+
 }
