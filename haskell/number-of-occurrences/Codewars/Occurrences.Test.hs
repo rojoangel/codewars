42c696965def5b6160a0298642cfeac223711935
rojoangel <rojo.angel@gmail.com>
2015-09-10 13:03:39 +0200

    haskell - number-of-occurrences - it. 2 - solved using filter

--- haskell/number-of-occurrences/Codewars/Occurrences.Test.hs
+++ haskell/number-of-occurrences/Codewars/Occurrences.Test.hs
@@ -2,11 +2,10 @@ module Codewars.Occurrences.Test where
 import Codewars.Occurrences (numberOfOccurrences)
 import Test.Hspec
 
-main = hspec $ do
+main = hspec $
   describe "numberOfOccurrences" $ do
-    it "should work on the empty list" $ do
-      property $ \x ->
-        numberOfOccurrences (x :: Int) [] `shouldBe` 0
+    it "should work on the empty list" $
+        numberOfOccurrences (9 :: Int) [] `shouldBe` 0
     it "should work for some examples" $ do
       let sample = [4, 0, 4]
       numberOfOccurrences 4 sample `shouldBe` 2
--- haskell/number-of-occurrences/Codewars/Occurrences.hs
+++ haskell/number-of-occurrences/Codewars/Occurrences.hs
@@ -1,4 +1,4 @@
 module Codewars.Occurrences where
 
 numberOfOccurrences :: Eq a => a -> [a] -> Int
-numberOfOccurrences x xs = error "todo: numberOfOccurrences"
+numberOfOccurrences x xs = length $ filter (x ==) xs
