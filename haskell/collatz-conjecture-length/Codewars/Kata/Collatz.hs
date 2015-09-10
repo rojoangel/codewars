5eac2381f487a69c51a268e4f3057b6ef17cb599
rojoangel <rojo.angel@gmail.com>
2015-09-10 12:30:54 +0200

    haskell - collatz-conjecture-length - it. 4 - refactor

--- haskell/collatz-conjecture-length/Codewars/Kata/Collatz.hs
+++ haskell/collatz-conjecture-length/Codewars/Kata/Collatz.hs
@@ -4,5 +4,6 @@ collatz :: Integer -> Int
 collatz = length . collatzSequence
 
 collatzSequence :: Integer -> [Integer]
-collatzSequence 1 = [1]
-collatzSequence 2 = 2 : collatzSequence (2 `div` 2)
+collatzSequence n
+  | even n      =   n : collatzSequence (n `div` 2)
+  | otherwise  = [1]
