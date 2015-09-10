5db14ce8615098ab599d3d914abe96a6b030ad65
rojoangel <rojo.angel@gmail.com>
2015-09-10 11:42:13 +0200

    haskell - esrever - it. 1 . initial commit

---
+++ haskell/esrever/README.md
@@ -0,0 +1,1 @@
+Write a function ```reverse``` which reverses a list (or in clojure's case, any list-like data structure)
---
+++ haskell/esrever/Reverse.hs
@@ -0,0 +1,6 @@
+module Reverse where
+import Prelude hiding (reverse)
+
+-- | Reverse a list
+reverse :: [a] -> [a]
+-- TODO: Program me
