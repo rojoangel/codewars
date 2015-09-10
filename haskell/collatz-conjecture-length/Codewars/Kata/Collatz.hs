051e7811f8f76045d5e4236453e24c8ce6aa2082
rojoangel <rojo.angel@gmail.com>
2015-09-10 12:36:52 +0200

    haskell - collatz-conjecture-length - it. 6 - refactor: encapsulate sequence generation

--- haskell/collatz-conjecture-length/Codewars/Kata/Collatz.hs
+++ haskell/collatz-conjecture-length/Codewars/Kata/Collatz.hs
@@ -2,9 +2,7 @@ module Codewars.Kata.Collatz where
 
 collatz :: Integer -> Int
 collatz = length . collatzSequence
-
-collatzSequence :: Integer -> [Integer]
-collatzSequence n
-  | n == 1     = [1]
-  | even n     = n : collatzSequence (n `div` 2)
-  | otherwise  = n : collatzSequence (n * 3 + 1)
+  where collatzSequence n
+          | n == 1     = [1]
+          | even n     = n : collatzSequence (n `div` 2)
+          | otherwise  = n : collatzSequence (n * 3 + 1)
