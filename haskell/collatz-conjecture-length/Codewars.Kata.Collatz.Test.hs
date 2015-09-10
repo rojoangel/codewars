28c25267caaa5987818f7fa3a46243116cb74588
rojoangel <rojo.angel@gmail.com>
2015-09-10 12:02:58 +0200

    haskell - collatz-conjecture-length - it. 1 - initial commit

---
+++ haskell/collatz-conjecture-length/Codewars.Kata.Collatz.Test.hs
@@ -0,0 +1,11 @@
+module Codewars.Kata.Collatz.Test where
+import Codewars.Kata.Collatz (collatz)
+import Test.Hspec
+
+main = hspec $ do
+  describe "collatz length" $ do
+    it "should work for some examples" $ do
+      collatz  1 `shouldBe`  1
+      collatz  2 `shouldBe`  2
+      collatz 15 `shouldBe` 18
+      collatz 20 `shouldBe`  8
---
+++ haskell/collatz-conjecture-length/Codewars.Kata.Collatz.hs
@@ -0,0 +1,4 @@
+module Codewars.Kata.Collatz where
+
+collatz :: Integer -> Int
+collatz n = error "todo: collatz"
