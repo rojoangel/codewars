4c20d153199e2eb59e907e4f09e75c16292cab17
rojoangel <rojo.angel@gmail.com>
2015-09-14 21:43:42 +0200

    haskell - sort-the-gift-code - it. 3 - solved using sort

--- haskell/sort-the-gift-code/SortGiftCode.hs
+++ haskell/sort-the-gift-code/SortGiftCode.hs
@@ -1,7 +1,5 @@
 module SortGiftCode where
+import Data.List (sort)
 
 sortGiftCode :: String -> String
-sortGiftCode []          = []
-sortGiftCode (x:xs)      = smallerSorted ++ [x] ++ biggerSorted -- using quicksort
-  where smallerSorted    = sortGiftCode [s | s <- xs, s < x]
-        biggerSorted     = sortGiftCode [s | s <- xs, s > x]
+sortGiftCode = sort
