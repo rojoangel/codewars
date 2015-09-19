30b53615065d1de10b5638fef6dffde0ad235b7d
rojoangel <rojo.angel@gmail.com>
2015-09-19 21:43:09 +0200

    haskell - title-case - it. 3 - add tests for no minors

--- haskell/title-case/TitleCase.Test.hs
+++ haskell/title-case/TitleCase.Test.hs
@@ -4,7 +4,13 @@ import Test.Hspec
 
 main :: IO ()
 main = hspec $
-  describe "titleCase" $
+  describe "titleCase" $ do
     it "should return empty string for empty string in" $ do
       titleCase "" "" `shouldBe` ""
       titleCase "anything a an" "" `shouldBe` ""
+    it "given no minors should capitalize all UPPERCASE" $
+      titleCase "" "THE WIND IN THE WILLOWS" `shouldBe` "The Wind In The Willows"
+    it "given no minors should capitalize all lowercase" $
+      titleCase "" "the quick brown fox" `shouldBe` "The Quick Brown Fox"
+    it "given no minors should capitalize mixed case" $
+      titleCase "" "a clash of KINGS" `shouldBe` "A Clash Of Kings"
--- haskell/title-case/TitleCase.hs
+++ haskell/title-case/TitleCase.hs
@@ -1,4 +1,8 @@
 module TitleCase (titleCase) where
 
+import qualified Data.Char as Char
+
 titleCase :: String -> String -> String
-titleCase _ [] = []
+titleCase _ xs = unwords . map toTitleCase $ words xs
+  where toTitleCase []      = []
+        toTitleCase (y:ys)  = Char.toUpper y : map Char.toLower ys
