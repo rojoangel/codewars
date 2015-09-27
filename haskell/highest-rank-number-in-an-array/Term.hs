bb83ec2f343d0d1ae1fac9e35f182190f8f49529
rojoangel <rojo.angel@gmail.com>
2015-09-27 11:59:26 +0200

    haskell - highest-rank-number-in-an-array - it. 2 - initial solution

--- haskell/highest-rank-number-in-an-array/Term.hs
+++ haskell/highest-rank-number-in-an-array/Term.hs
@@ -2,3 +2,4 @@ module Term where
 import Data.List
 
 highestRank :: Ord c => [c] -> c
+highestRank xs = fst . foldl1 (\acc@(x',y') d@(x,y) -> if y >= y' then d else acc) . map (\l@(x:xs) -> (x,length l)) . group . sort $ xs
