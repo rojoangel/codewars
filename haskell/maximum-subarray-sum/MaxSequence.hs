06027e9694ea2b1deb32b987d5da5d72a71635dc
rojoangel <rojo.angel@gmail.com>
2015-09-27 20:05:53 +0200

    haskell - maximum-subarray-sum - it. 2 - initial solution using concatMap

--- haskell/maximum-subarray-sum/MaxSequence.hs
+++ haskell/maximum-subarray-sum/MaxSequence.hs
@@ -5,3 +5,5 @@ import Data.Ord (comparing)
 
 -- Return the greatest subarray sum within the array of integers passed in.
 maxSequence :: [Int] -> Int
+maxSequence xs = sum $ maximumBy (comparing sum) (contiguousSubsequences xs)
+  where contiguousSubsequences = ([]:) . concatMap (tail . inits) . tails
