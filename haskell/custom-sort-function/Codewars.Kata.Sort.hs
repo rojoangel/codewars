7457659db42cf0eb1e341fbf8f66e897d5dccb03
rojoangel <rojo.angel@gmail.com>
2015-10-03 22:31:17 +0200

    haskell - custom-sort-fuction - it. 1 - initial solution using list comprehension - unable to commit to codewars due to issues with tests

--- haskell/custom-sort-function/Codewars.Kata.Sort.hs
+++ haskell/custom-sort-function/Codewars.Kata.Sort.hs
@@ -1,4 +1,7 @@
 module Codewars.Kata.Sort where
 
 sort :: Ord a => [a] -> [a]
-sort xs = error "todo: sort"
+sort []     = []
+sort (x:xs) = smallerSorted ++ [x] ++ greaterSorted
+  where smallerSorted = sort [x' | x' <- xs, x' <= x]
+        greaterSorted = sort [x' | x' <- xs, x' >  x]
