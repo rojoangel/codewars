1b0f5f082ebb37fae5a105e136153a206b3d2005
rojoangel <rojo.angel@gmail.com>
2015-10-03 22:33:45 +0200

    haskell - custom-sort-fuction - it. 3 - remove duplicated files

--- haskell/custom-sort-function/Codewars/Kata/Sort.hs
+++
@@ -1,6 +0,0 @@
-module Codewars.Kata.Sort where
-
-  sort :: Ord a => [a] -> [a]
-  sort (x:xs) = smallerSorted ++ [x] ++ greaterSorted
-    where smallerSorted = sort [x' | x' <- xs, x' <= x]
-          greaterSorted = sort [x' | x' <- xs, x' >  x]
