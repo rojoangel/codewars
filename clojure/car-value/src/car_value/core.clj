c56f579111d2d2ddab945411810ea41e765262d4
rojoangel <rojo.angel@gmail.com>
2015-10-09 23:05:45 +0200

    clojure car-value - it. 3 - first solution - using loop

--- clojure/car-value/src/car_value/core.clj
+++ clojure/car-value/src/car_value/core.clj
@@ -1,5 +1,19 @@
 (ns car-value.core)
 
-(defn car [price years]
-  (format "%.2f" (* price 0.8))
+(defn depreciation
+  [year]
+  (if (< year 3)
+    0.20
+    0.10)
+  )
+
+(defn car
+  [price years]
+  (loop [depreciated-value price
+         year 1]
+    (if (= year years)
+      (format "%.2f" (double (* depreciated-value (- 1 (depreciation year)))))
+      (recur (* depreciated-value (- 1 (depreciation year))) (inc year))
+      )
+    )
   )
