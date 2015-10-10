597992e255f2430ef5da171b9638da17f0a17ec9
rojoangel <rojo.angel@gmail.com>
2015-10-10 16:16:03 +0200

    clojure - barycenter - it. 2 - initial solution

--- clojure/barycenter/src/barycenter/core.clj
+++ clojure/barycenter/src/barycenter/core.clj
@@ -1,4 +1,9 @@
 (ns barycenter.core)
 
+(defn round-ratio
+  [ratio]
+  (double (/ (Math/round (* 10000 (double ratio))) 10000))
+  )
+
 (defn bar-triang [[a b] [c d] [e f]]
-  ; your code)
+  [(round-ratio (/ (+ a c e) 3)) , (round-ratio (/ (+ b d f) 3))])
