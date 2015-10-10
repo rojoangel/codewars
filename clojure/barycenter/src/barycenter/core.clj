7215da6d8a592d41b7a28371d2bc93d05d54f7af
rojoangel <rojo.angel@gmail.com>
2015-10-10 16:24:15 +0200

    clojure - barycenter - it. 3 - refactor - extract reusable functions

--- clojure/barycenter/src/barycenter/core.clj
+++ clojure/barycenter/src/barycenter/core.clj
@@ -2,8 +2,12 @@
 
 (defn round-ratio
   [ratio]
-  (double (/ (Math/round (* 10000 (double ratio))) 10000))
-  )
+  (double (/ (Math/round (* 10000 (double ratio))) 10000)))
 
-(defn bar-triang [[a b] [c d] [e f]]
-  [(round-ratio (/ (+ a c e) 3)) , (round-ratio (/ (+ b d f) 3))])
+(defn bar-coordinate
+  [c1 c2 c3]
+  (round-ratio (/ (+ c1 c2 c3) 3)))
+
+(defn bar-triang
+  [[a b] [c d] [e f]]
+  [(bar-coordinate a c e) , (bar-coordinate b d f)])
