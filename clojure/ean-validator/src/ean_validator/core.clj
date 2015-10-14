6f23e42940391abd64709857ccce23471659b1ed
rojoangel <rojo.angel@gmail.com>
2015-10-14 22:35:17 +0200

    clojure - ean-validator - it. 1 - initial solution

--- clojure/ean-validator/src/ean_validator/core.clj
+++ clojure/ean-validator/src/ean_validator/core.clj
@@ -1,5 +1,18 @@
 (ns ean-validator.core)
 
 (defn validate-ean [ean-code]
-  ; Your code
+  (== 0
+      (mod
+       (- 10
+          (mod
+           (reduce +
+                   (map *
+                        (map #(- (int %) 48) (seq (str ean-code)))
+                        (flatten (repeat (list* 1 3 [1 3])))
+                        )
+                   )
+           10)
+          )
+       10)
+      )
   )
