124f9c8f44956b7160f28b685b7b920731477843
rojoangel <rojo.angel@gmail.com>
2015-10-17 13:35:37 +0200

    clojure - comet-fan - it. 1 - initial commit

---
+++ clojure/comet-fan/src/comet_fan/core.clj
@@ -0,0 +1,6 @@
+(ns comet-fan.core)
+
+(defn ride
+  "Returns GO if the group gets to go and STAY otherwise"
+  [group comet]
+  "GO")
---
+++ clojure/comet-fan/test/comet_fan/core_test.clj
@@ -0,0 +1,3 @@
+(ns comet-fan.core-test
+  (:require [clojure.test :refer :all]
+            [comet-fan.core :refer :all]))
