c04779d21c3013abeb3459d4ce98761e587cdcb1
rojoangel <rojo.angel@gmail.com>
2015-10-17 12:39:47 +0200

    clojure - digital-root - it. 2 - digital-root of a single digit should be the digit

--- clojure/digital-root/test/digital_root/core_test.clj
+++ clojure/digital-root/test/digital_root/core_test.clj
@@ -2,5 +2,12 @@
   (:require [clojure.test :refer :all]
             [digital-root.core :refer :all]))
 
+(comment
 (deftest digital-root-example-test
   (is (= (digital-root 16) 7)))
+)
+
+(deftest single-digit
+  (testing "digital-root of a single digit should be the digit"
+    (is (= (digital-root 9) 9)))
+  )
