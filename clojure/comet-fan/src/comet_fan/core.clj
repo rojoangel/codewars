84d334dfa6c5f3273f45cc5a80695de342f89e61
rojoangel <rojo.angel@gmail.com>
2015-10-17 19:10:39 +0200

    clojure - comet-fan - it. 2 - tests for single letter names

--- clojure/comet-fan/src/comet_fan/core.clj
+++ clojure/comet-fan/src/comet_fan/core.clj
@@ -1,5 +1,9 @@
 (ns comet-fan.core)
 
+(defn to-number
+  [name]
+  (reduce * (map #(- (int %) 64) name)))
+
 (defn ride
   "Returns GO if the group gets to go and STAY otherwise"
   [group comet]
--- clojure/comet-fan/test/comet_fan/core_test.clj
+++ clojure/comet-fan/test/comet_fan/core_test.clj
@@ -1,3 +1,10 @@
 (ns comet-fan.core-test
   (:require [clojure.test :refer :all]
             [comet-fan.core :refer :all]))
+
+(deftest name-to-number
+  (testing "single letter value should return its value"
+    (is (= (to-number "A") 1)))
+  (testing "a different single letter value should return a different value"
+    (is (= (to-number "Z") 26)))
+  )
