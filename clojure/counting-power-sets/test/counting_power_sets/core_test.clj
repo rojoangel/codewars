86aefa7e7e6cacf6b2d13601f82aa7b35ca2e5e5
rojoangel <rojo.angel@gmail.com>
2015-10-22 22:28:32 +0200

    clojure - counting-power-sets - it. 2 - initial solution

--- clojure/counting-power-sets/test/counting_power_sets/core_test.clj
+++ clojure/counting-power-sets/test/counting_power_sets/core_test.clj
@@ -2,3 +2,10 @@
   (:require [clojure.test :refer :all]
             [counting-power-sets.core :refer :all]))
 
+(deftest a-test1
+  (testing "Basic tests"
+    (is (= 1 (powers [])))
+    (is (= 2 (powers [1])))
+    (is (= 4 (powers [1 2])))
+    (is (= 8 (powers [1 2 3])))
+    (is (= (powers (range 100)) 1267650600228229401496703205376N))))
