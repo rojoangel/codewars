b2a7d1af0f79bbcb9e51223c77bb2d9cea39be6b
rojoangel <rojo.angel@gmail.com>
2015-10-22 21:56:19 +0200

    clojure - counting-power-sets - it. 1 - initial commit

---
+++ clojure/counting-power-sets/src/counting_power_sets/core.clj
@@ -0,0 +1,5 @@
+(ns counting-power-sets.core)
+
+(defn powers [list]
+  ; Program me!
+  )
---
+++ clojure/counting-power-sets/test/counting_power_sets/core_test.clj
@@ -0,0 +1,4 @@
+(ns counting-power-sets.core-test
+  (:require [clojure.test :refer :all]
+            [counting-power-sets.core :refer :all]))
+
