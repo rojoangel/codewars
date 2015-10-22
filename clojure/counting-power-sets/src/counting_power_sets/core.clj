20c515c7c57594a5be2858a72e01fc8309bd7c3b
rojoangel <rojo.angel@gmail.com>
2015-10-22 22:39:42 +0200

    clojure - counting-power-sets - it. 3 - inlined exponent function

--- clojure/counting-power-sets/src/counting_power_sets/core.clj
+++ clojure/counting-power-sets/src/counting_power_sets/core.clj
@@ -2,5 +2,4 @@
 
 ;; sum of binomial coefficients = 2^n (http://math.stackexchange.com/questions/734900/proof-by-induction-sum-of-binomial-coefficients-sum-k-0n-n-k-2n)
 (defn powers [list]
-  (letfn [(exp [x n] (reduce * (repeat n (bigint x))))]
-    (exp 2 (count list))))
+  (reduce * (repeat (count list) 2N)))
