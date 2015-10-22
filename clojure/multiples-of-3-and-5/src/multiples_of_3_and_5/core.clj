1a0583411cc0bcd5d960e9d293390a7bd2c3038b
rojoangel <rojo.angel@gmail.com>
2015-10-22 21:07:00 +0200

    clojure - multiples-of-3-and-5 - it. 3 - refactor to use zero?

--- clojure/multiples-of-3-and-5/src/multiples_of_3_and_5/core.clj
+++ clojure/multiples-of-3-and-5/src/multiples_of_3_and_5/core.clj
@@ -1,4 +1,4 @@
 (ns multiples-of-3-and-5.core)
 
 (defn solution [number]
-  (reduce + (filter #(or (= (mod % 5) 0) (= (mod % 3) 0)) (range number))))
+  (reduce + (filter #(or (zero? (mod % 5)) (zero? (mod % 3))) (range number))))
