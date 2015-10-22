8706585aab3549135b45af10fbe24356df86c005
rojoangel <rojo.angel@gmail.com>
2015-10-22 22:47:38 +0200

    clojure - counting-power-sets - it. 4 - refactored using .pow & biginteger

--- clojure/counting-power-sets/src/counting_power_sets/core.clj
+++ clojure/counting-power-sets/src/counting_power_sets/core.clj
@@ -2,4 +2,4 @@
 
 ;; sum of binomial coefficients = 2^n (http://math.stackexchange.com/questions/734900/proof-by-induction-sum-of-binomial-coefficients-sum-k-0n-n-k-2n)
 (defn powers [list]
-  (reduce * (repeat (count list) 2N)))
+  (.pow (biginteger 2) (count list)))
