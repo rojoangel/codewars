2ae81e5e0c9903d5a0c127f0fa9bc302c6b44e86
rojoangel <rojo.angel@gmail.com>
2015-10-23 20:20:47 +0200

    clojure - sq-in-rect - initial solution

--- clojure/sq-in-rect/src/sq_in_rect/core.clj
+++ clojure/sq-in-rect/src/sq_in_rect/core.clj
@@ -1,5 +1,13 @@
 (ns sq-in-rect.core)
 
-(defn sq-in-rect [lng wdth]
-  ; your code
-)
+(defn sq-in-rect
+  ([lng wdth]
+   (if (= lng wdth)
+     nil
+    (sq-in-rect lng wdth [])))
+  ([lng wdth accumulated]
+    (if (= lng wdth)
+      (conj accumulated lng)
+      (recur (- (max lng wdth) (min lng wdth))
+             (min lng wdth)
+             (conj accumulated (min lng wdth))))))
